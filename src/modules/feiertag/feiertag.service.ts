3e804e359528aa1ef5f19a841e0f642a53a69578
Polina <polina_sch@web.de>
2020-11-07 19:59:02 +0100

    edit deleteFeiertag Service

--- src/modules/feiertag/feiertag.service.ts
+++ src/modules/feiertag/feiertag.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, DeleteResult } from 'typeorm';
 import { Feiertag } from './feiertag.entity';
 
 @Injectable()
@@ -14,11 +14,13 @@ export class FeiertagService {
         return this.feiertagRepository.find();
       }
     
-      findFeiertagByID(id: string): Promise<Feiertag> {
+      findFeiertagByID(id: number): Promise<Feiertag> {
         return this.feiertagRepository.findOne(id);
       }
     
-      async deleteFeiertag(id: string): Promise<void> {
-        await this.feiertagRepository.delete(id);
+      async deleteBundesland(id: number): Promise<DeleteResult> {
+        return await this.feiertagRepository.findOne(id).then((value) => {
+          return this.feiertagRepository.delete(value);
+        });
       }
 }
