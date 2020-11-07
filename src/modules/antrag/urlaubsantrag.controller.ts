11166e8679bad28b61a36d5e6dc7b8b85ebc49be
Polina <polina_sch@web.de>
2020-11-08 00:14:00 +0100

    Create Urlaubsantrag Controller

--- src/modules/antrag/urlaubsantrag.controller.ts
+++ src/modules/antrag/urlaubsantrag.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Body, Param, HttpStatus, ParseIntPipe } from '@nestjs/common';
+import { CreateUrlaubsantrag, UpdateUrlaubsantrag } from './dto';
+import { Urlaubsantrag } from './urlaubsantrag.entity';
+import { UrlaubsantragService } from './urlaubsantrag.service';
 
 @Controller('urlaubsantrag')
-export class UrlaubsantragController {}
+export class UrlaubsantragController {
+    constructor(private readonly urlaubsantragService: UrlaubsantragService) {}
+
+    @Post()
+    async createUrlaubsantrag(@Body() createUrlaubsantrag: CreateUrlaubsantrag): Promise<Urlaubsantrag> {
+    return this.urlaubsantragService.createAntrag(createUrlaubsantrag);
+    }
+
+    @Get()
+    async getUrlaubsantraege(): Promise<Urlaubsantrag[]> {
+        return this.urlaubsantragService.getAllAntraege();
+    }
+
+    @Get(':id')
+    async getUrlaubsantragById(@Param('id', new ParseIntPipe()) id): Promise<Urlaubsantrag> {
+        return this.urlaubsantragService.findAntragByID(id);
+    }
+
+    @Patch()
+    async update(@Body() updateUrlaubsantrag: UpdateUrlaubsantrag) {
+    return this.urlaubsantragService.updateAntrag(updateUrlaubsantrag);
+  }
+
+    @Delete(':id')
+    async remove(@Param('id', new ParseIntPipe()) id) {
+    return this.urlaubsantragService.deleteAntrag(id);
+  }
+}
