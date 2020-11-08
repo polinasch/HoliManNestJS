7ccc67878b7bd5a49a3e88e0cac958b0165563b3
Polina <polina_sch@web.de>
2020-11-08 19:02:58 +0100

    Add Benutzer Controller

--- src/modules/benutzer/benutzer.controller.ts
+++ src/modules/benutzer/benutzer.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Body, Param, ParseIntPipe } from '@nestjs/common';
+import { Benutzer } from './benutzer.entity';
+import { BenutzerService } from './benutzer.service';
+import { CreateBenutzer, UpdateBenutzer } from './dto';
 
 @Controller('benutzer')
-export class BenutzerController {}
+export class BenutzerController {
+    constructor(private readonly benutzerService: BenutzerService) {}
+
+    @Post()
+    async createBenutzer(@Body() createBenutzer: CreateBenutzer): Promise<Benutzer> {
+    return this.benutzerService.createBenutzer(createBenutzer);
+    }
+
+    @Get()
+    async getBenutzer(): Promise<Benutzer[]> {
+        return this.benutzerService.getAllBenutzer();
+    }
+
+    @Get(':id')
+    async getBenutzerByID(@Param('id', new ParseIntPipe()) id): Promise<Benutzer> {
+        return this.benutzerService.findBenutzerByID(id);
+    }
+
+    @Patch()
+    async update(@Body() updateBenutzer: UpdateBenutzer) {
+    return this.benutzerService.updateBenutzer(updateBenutzer);
+  }
+
+    @Delete(':id')
+    async remove(@Param('id', new ParseIntPipe()) id) {
+    return this.benutzerService.deleteBenutzer(id);
+  }
+}
