f8ca84d6ae5e0b55e68937581d116bf39e0f64b7
Polina <polina_sch@web.de>
2020-11-08 18:59:04 +0100

    Add Arbeitstage Controller

--- src/modules/arbeitstage/arbeitstage.controller.ts
+++ src/modules/arbeitstage/arbeitstage.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Body, Param, ParseIntPipe } from '@nestjs/common';
+import { Arbeitstage } from './arbeitstage.entity';
+import { ArbeitstageService } from './arbeitstage.service';
+import { CreateArbeitstage, UpdateArbeitstage } from './dto';
 
 @Controller('arbeitstage')
-export class ArbeitstageController {}
+export class ArbeitstageController {
+    constructor(private readonly arbeitstageService: ArbeitstageService) {}
+
+    @Post()
+    async createArbeitstage(@Body() createArbeitstage: CreateArbeitstage): Promise<Arbeitstage> {
+    return this.arbeitstageService.createArbeitstage(createArbeitstage);
+    }
+
+    @Get()
+    async getArbeitstage(): Promise<Arbeitstage[]> {
+        return this.arbeitstageService.getAllArbeitstage();
+    }
+
+    @Get(':id')
+    async getArbeitstageByID(@Param('id', new ParseIntPipe()) id): Promise<Arbeitstage> {
+        return this.arbeitstageService.findArbeitstageByID(id);
+    }
+
+    @Patch()
+    async update(@Body() updateArbeitstage: UpdateArbeitstage) {
+    return this.arbeitstageService.updateArbeitstage(updateArbeitstage);
+  }
+
+    @Delete(':id')
+    async remove(@Param('id', new ParseIntPipe()) id) {
+    return this.arbeitstageService.deleteArbeitstage(id);
+  }
+}
