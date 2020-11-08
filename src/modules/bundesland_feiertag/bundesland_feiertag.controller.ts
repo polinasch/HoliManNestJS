79f5d81fa8ca545ec9c6c8436b4dcecfa8f4ff1f
Polina <polina_sch@web.de>
2020-11-08 19:09:02 +0100

    Add BLFeiertag Controller

--- src/modules/bundesland_feiertag/bundesland_feiertag.controller.ts
+++ src/modules/bundesland_feiertag/bundesland_feiertag.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Body, Param, ParseIntPipe } from '@nestjs/common';
+import { BundeslandFeiertag } from './bundesland_feiertag.entity';
+import { BundeslandFeiertagService } from './bundesland_feiertag.service';
+import { CreateBundeslandFeiertag, UpdateBundeslandFeiertag } from './dto';
 
 @Controller('bundesland')
-export class BundeslandFeiertagController {}
+export class BundeslandFeiertagController {
+    constructor(private readonly bundeslandFeiertagService: BundeslandFeiertagService) {}
+
+    @Post()
+    async createBundeslandFeiertag(@Body() createBundeslandFeiertag: CreateBundeslandFeiertag): Promise<BundeslandFeiertag> {
+    return this.bundeslandFeiertagService.createBLFeiertag(createBundeslandFeiertag);
+    }
+
+    @Get()
+    async getBundeslandFeiertag(): Promise<BundeslandFeiertag[]> {
+        return this.bundeslandFeiertagService.getAllBLFeiertage();
+    }
+
+    @Get(':id')
+    async getBundeslandFeiertagByID(@Param('id', new ParseIntPipe()) id): Promise<BundeslandFeiertag> {
+        return this.bundeslandFeiertagService.findBLFeiertagByID(id);
+    }
+
+    @Patch()
+    async update(@Body() updateBundeslandFeiertag: UpdateBundeslandFeiertag) {
+    return this.bundeslandFeiertagService.updateBLFeiertag(updateBundeslandFeiertag);
+  }
+
+    @Delete(':id')
+    async remove(@Param('id', new ParseIntPipe()) id) {
+    return this.bundeslandFeiertagService.deleteBLFeiertag(id);
+  }
+}
