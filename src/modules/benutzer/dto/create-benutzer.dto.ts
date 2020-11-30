354b67894bd058f2cf39d53c4ec587e2695a8efe
Polina <polina_sch@web.de>
2020-11-30 17:40:11 +0100

    add class validator

--- src/modules/benutzer/dto/create-benutzer.dto.ts
+++ src/modules/benutzer/dto/create-benutzer.dto.ts
@@ -1,9 +1,20 @@
 import { Bundesland } from '../../bundesland/bundesland.entity';
 import { Arbeitstage } from '../../arbeitstage/arbeitstage.entity';
+import {IsString, Matches, MaxLength, MinLength} from 'class-validator';
 
 export class CreateBenutzer {
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
     readonly Benutzername: string;
+
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
+    //@Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, 
+    //{message: 'das Passwort ist zu schwach'})
     readonly Passwort: string;
+
     readonly istAdmin: boolean;
     readonly istVorgesetzter: boolean;
     readonly Vorgesetzter: string;
