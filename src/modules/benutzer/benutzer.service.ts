84a50c47ec34ab8bcdbb135aaf9c2eebb54a1c3b
Polina <polina_sch@web.de>
2020-12-27 16:58:47 +0100

    replace update method with save method

--- src/modules/benutzer/benutzer.service.ts
+++ src/modules/benutzer/benutzer.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository, DeleteResult } from 'typeorm';
 import { Benutzer } from './benutzer.entity';
@@ -6,37 +6,42 @@ import { CreateBenutzer } from './dto/index';
 
 @Injectable()
 export class BenutzerService {
-    constructor(
-        @InjectRepository(Benutzer)
-        private benutzerRepository: Repository<Benutzer>,
-      ) {}
-    
-      getAllBenutzer(): Promise<Benutzer[]> {
-        return this.benutzerRepository.find({ relations: ['bundesland', 'arbeitstage', 'anträge']});
-      }
-    
-      findBenutzerByID(id: number): Promise<Benutzer> {
-        return this.benutzerRepository.findOne(id, { relations: ['bundesland', 'arbeitstage', 'anträge']});
-      }
+  constructor(
+    @InjectRepository(Benutzer)
+    private benutzerRepository: Repository<Benutzer>,
+  ) { }
 
-      getAllVorgesetzten(istVorgesetzter: boolean): Promise<Benutzer[]>{
-        return this.benutzerRepository.find({ where: {istVorgesetzter: istVorgesetzter} });
-      }
+  getAllBenutzer(): Promise<Benutzer[]> {
+    return this.benutzerRepository.find({ relations: ['bundesland', 'arbeitstage', 'anträge'] });
+  }
 
-      async createBenutzer(benutzer: CreateBenutzer) {
-        const nutzer = this.benutzerRepository.create(benutzer);
-        await this.benutzerRepository.save(benutzer);
-        return nutzer;
-      }
+  findBenutzerByID(id: number): Promise<Benutzer> {
+    return this.benutzerRepository.findOne(id, { relations: ['bundesland', 'arbeitstage', 'anträge'] });
+  }
 
-      async updateBenutzer(id: number, createBenutzerDTO: CreateBenutzer) {
-        await this.benutzerRepository.update( id, createBenutzerDTO);
-        return await this.benutzerRepository.findOne(id);
-      }
-    
-      async deleteBenutzer(id: number): Promise<DeleteResult> {
-        return await this.benutzerRepository.findOne(id).then((value) => {
-          return this.benutzerRepository.delete(value);
-        });
-      }
+  async findBenutzerByVorgesetzter(id: number): Promise<Benutzer[]> {
+    return this.benutzerRepository.find({ where: { Vorgesetzter: id } });
+  }
+
+  getAllVorgesetzten(istVorgesetzter: boolean): Promise<Benutzer[]> {
+    return this.benutzerRepository.find({ where: { istVorgesetzter: istVorgesetzter } });
+  }
+
+  async createBenutzer(benutzer: CreateBenutzer) {
+    const nutzer = this.benutzerRepository.create(benutzer);
+    await this.benutzerRepository.save(benutzer);
+    return nutzer;
+  }
+
+  async updateBenutzer(id: number, createBenutzerDTO: CreateBenutzer) {
+    let benutzerUpdate = await this.benutzerRepository.findOne({ BenutzerID: id });
+    if (!benutzerUpdate) throw new NotFoundException({ message: "Benutzer existiert nicht" });
+    return this.benutzerRepository.save({ ...benutzerUpdate, ...createBenutzerDTO });
+  }
+
+  async deleteBenutzer(id: number): Promise<DeleteResult> {
+    return await this.benutzerRepository.findOne(id).then((value) => {
+      return this.benutzerRepository.delete(value);
+    });
+  }
 }
